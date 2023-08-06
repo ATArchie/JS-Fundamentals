57e07af6dbea6d4ef428679536768f2bfa656f73
Arkadiusz Tomalski <arkadiusz.tomalski@g2a.com>
2023-08-06 14:57:13 +0200

    first lesson

---
+++ lessons/lesson1.js
@@ -0,0 +1,25 @@
+// Hello World
+console.log ("Hello World")
+
+// Variables 
+var firstName = "John"
+let lastName = "Doe"
+console.log(lastName)
+
+var age, dateOfBirth, sex
+age = "20"
+sex = "male"
+console.log(age)
+age = "21"
+console.log(age)
+
+// constants
+const occupation = "Tester"
+console.log(occupation)
+
+// data types
+var middleName = 'Dick' //string
+var ageOfBrother = 22 //number
+var isHeMarried = false //boolean
+var yearsInMarriage = null //null
+var numberOfCars = undefined //undefined
