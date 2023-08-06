3610851fda0e723927de98048446832bbc4b20d8
Arkadiusz Tomalski <arkadiusz.tomalski@g2a.com>
2023-08-06 15:30:29 +0200

    lesson4

---
+++ lessons/lesson4.js
@@ -0,0 +1,15 @@
+//Relational or comparison operators
+
+// > more than
+// < less than
+// >= more than equal
+// <= less then equal
+
+var result = 1 < 5
+//console.log(result)
+
+// equality operators
+
+var x = 1
+console.log(x == '1') // lose comparison
+console.log(x === '1') // strict comparison - 1 nie równa się '1' bo '1' jest stringiem a nie number
